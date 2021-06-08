21879b173031747f7dae65890589269647f46208
Oleg D <draqgunkin-ov@ural.rt.ru>
2021-06-08 16:11:28 +0500

    Add comentary

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!!!");
+            Console.WriteLine("For Git new");
             Console.ReadKey();
         }
     }
